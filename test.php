40d4fcbad4bb35312982c02f9d37e6389485adb9
Ferdinand Frost <f.frost@ecentral.de>
2023-09-27 12:56:19 +0200

    Remove empty line

--- test.php
+++ test.php
@@ -5,25 +5,24 @@
  *
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
- * 
  */
- /**
- * required methods:
- * ---- assets ------
- * - get asset with metadata
- * - delete asset
- * - update assets metadata
- * - get download asset link
- * - get all metadata definitions (maybe?)
- * - download asset
- * - upload file https://pixelboxx.pages.pixelboxx.io/docs/rest/docs/rest-api/assets/import-assets
- * - get thumbnail (maybe?)
- * - search
- * ---- folder ------
- * - get tree
- * - rename folder
- * - delete folder
- * - move folder to new parent folder
- * - get folder content
- * - create folder
- */
+/**
+* required methods:
+* ---- assets ------
+* - get asset with metadata
+* - delete asset
+* - update assets metadata
+* - get download asset link
+* - get all metadata definitions (maybe?)
+* - download asset
+* - upload file https://pixelboxx.pages.pixelboxx.io/docs/rest/docs/rest-api/assets/import-assets
+* - get thumbnail (maybe?)
+* - search
+* ---- folder ------
+* - get tree
+* - rename folder
+* - delete folder
+* - move folder to new parent folder
+* - get folder content
+* - create folder
+*/
