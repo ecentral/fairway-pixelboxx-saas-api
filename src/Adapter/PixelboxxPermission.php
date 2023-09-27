2320c73fdee2d41e8c946efc846bd083adc3ae60
Ferdinand Frost <f.frost@ecentral.de>
2023-09-27 12:49:23 +0200

    Update file copyright comment

--- src/Adapter/PixelboxxPermission.php
+++ src/Adapter/PixelboxxPermission.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the "fairway_api" library by eCentral GmbH.
+ * This file is part of the "fairway_pixelboxx_saas_api" library by eCentral GmbH.
  *
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
